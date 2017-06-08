a335ff9bed89d998b69b84bbc0fa3106303a1fed
sGIoG <christian.dabuchi@gmail.com>
2017-06-08 15:26:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<head>
+	<link rel=\"stylesheet\" href=\"stylesheet.css\" type=\"text/css\" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(window).load(function() {
+			$(".loader").fadeOut("slow");
+		})
+	</script>
+</head>
+
+<body>
+	<div class="loader"></div>
+	<h1>TEST</h1>
+</body>
